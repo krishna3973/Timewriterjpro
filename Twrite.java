e4349253b54b7900a2cab2a96a9fcd776dd4a166
krishna3973 <venkatakrishna3973@gmail.com>
2024-08-25 15:21:09 +0530

    milli multiplier

--- Twrite.java
+++ Twrite.java
@@ -2,10 +2,10 @@ public class Twrite{
 
     public static void main(String[] args) {
         
-        for(int i=0; i<10; i++){
+        for(int i=1; i<=10; i++){
             System.out.println(i);
             try {
-                Thread.sleep(1000);
+                Thread.sleep(i*100);
             } catch (Exception e) {
                 e.printStackTrace();
             }

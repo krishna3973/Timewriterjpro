d8c84ceeba292dea4d4173e5a52a96cd769617fa
krishna3973 <venkatakrishna3973@gmail.com>
2024-08-25 16:40:07 +0530

    for input test

--- Twrite.java
+++ Twrite.java
@@ -1,13 +1,14 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.util.Calendar;
+import java.util.Scanner;
 
 public class Twrite{
 
     public static void main(String[] args) {
 
-        boolean donu = false;
-        File fl = new File("file path");
+        boolean donu = true;
+        /*File fl = new File("file path");
         FileWriter fw = null;
 
         try{
@@ -15,24 +16,33 @@ public class Twrite{
             donu = true;
         }catch(Exception e){
             e.printStackTrace();
-        }
+        }*/
 
         Calendar ca;
         if(donu){
 
-            try{
+            Scanner scr = new Scanner(System.in);
+            int noofscd = scr.nextInt();
+            scr.close();
 
-                for(int i=1; i<=10; i++){
-                    ca = Calendar.getInstance();
-                    System.out.println(ca.get(Calendar.HOUR_OF_DAY)+":"+ca.get(Calendar.MINUTE)+":"+ca.get(Calendar.SECOND));
-                    fw.write((ca.get(Calendar.HOUR_OF_DAY)+":"+ca.get(Calendar.MINUTE)+":"+ca.get(Calendar.SECOND))+"\n");
-                    Thread.sleep(1000);
+            if(noofscd >0 && noofscd < 60){
+
+                try{
+
+                    for(int i=1; i<=noofscd; i++){
+                        ca = Calendar.getInstance();
+                        System.out.println(ca.get(Calendar.HOUR_OF_DAY)+":"+ca.get(Calendar.MINUTE)+":"+ca.get(Calendar.SECOND));
+                        //fw.write((ca.get(Calendar.HOUR_OF_DAY)+":"+ca.get(Calendar.MINUTE)+":"+ca.get(Calendar.SECOND))+"\n");
+                        Thread.sleep(1000);
+                    }
+
+                    //fw.close();
+
+                }catch(Exception e){
+                    e.printStackTrace();
                 }
-
-                fw.close();
-
-            }catch(Exception e){
-                e.printStackTrace();
+            }else{
+                System.out.println("INVALID INPUT");
             }
         }
 

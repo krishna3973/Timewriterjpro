ba830a3abf9d2eceb4f9c3211fa06194a4743e20
krishna3973 <venkatakrishna3973@gmail.com>
2024-08-25 15:57:40 +0530

    writing to file default

--- Twrite.java
+++ Twrite.java
@@ -1,18 +1,37 @@
+import java.io.File;
+import java.io.FileWriter;
 import java.util.Calendar;
 
 public class Twrite{
 
     public static void main(String[] args) {
 
-        Calendar ca;
-        
-        for(int i=1; i<=10; i++){
+        boolean donu = false;
+        File fl = new File("file path");
+        FileWriter fw = null;
 
-            ca = Calendar.getInstance();
-            System.out.println(ca.get(Calendar.HOUR_OF_DAY)+":"+ca.get(Calendar.MINUTE)+":"+ca.get(Calendar.SECOND));
-            try {
-                Thread.sleep(1000);
-            } catch (Exception e) {
+        try{
+            fw = new FileWriter(fl, true);
+            donu = true;
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        Calendar ca;
+        if(donu){
+
+            try{
+
+                for(int i=1; i<=10; i++){
+                    ca = Calendar.getInstance();
+                    System.out.println(ca.get(Calendar.HOUR_OF_DAY)+":"+ca.get(Calendar.MINUTE)+":"+ca.get(Calendar.SECOND));
+                    fw.write((ca.get(Calendar.HOUR_OF_DAY)+":"+ca.get(Calendar.MINUTE)+":"+ca.get(Calendar.SECOND))+"\n");
+                    Thread.sleep(1000);
+                }
+
+                fw.close();
+
+            }catch(Exception e){
                 e.printStackTrace();
             }
         }
